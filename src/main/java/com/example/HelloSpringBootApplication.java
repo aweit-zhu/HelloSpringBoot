b67edbffcc24a9e6ad395c8b8cfabeb990cf86f9
aweit-zhu <a4192696@yahoo.com.tw>
2025-10-23 19:06:53 +0800

    update yaml

--- src/main/java/com/example/HelloSpringBootApplication.java
+++ src/main/java/com/example/HelloSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBootApplication.class, args); 
+		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
 
 }
